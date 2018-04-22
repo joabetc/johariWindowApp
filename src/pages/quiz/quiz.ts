333767d2ac626a0d858d83294e3f8ec2670daa03
Joabe Costa <joabetc@hotmail.com>
2018-04-22 00:32:45 -0300

    Using mock data

--- src/pages/quiz/quiz.ts
+++ src/pages/quiz/quiz.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { QUESTIONS } from '../../model/mock-questions';
+import { Question } from '../../model/question';
 
 @Component({
   selector: 'page-quiz',
   templateUrl: 'quiz.html',
 })
 export class QuizPage {
+  questions: Question[] = QUESTIONS;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
