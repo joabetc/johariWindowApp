8e3fe94d274584a974ccea9bf2ef42de46c4a0e6
Joabe Costa <joabetc@hotmail.com>
2018-04-22 00:10:37 -0300

    Mock model data

---
+++ src/model/mock-questions.ts
@@ -0,0 +1,18 @@
+import { Question } from "./question";
+
+export const QUESTIONS: Question[] = [
+  {
+    id: 1,
+    text: 'Caso um colega tenha um mau relacionamento com outro colaborador, cuja cooperação é fundamental para o bom desempenho do grupo, eu...',
+    answers: [
+      {
+        id: 'A',
+        text: 'Falaria ao meu colega que ele é responsável em parte, pelo mau relacionamento com o outro e das consequências que isso está acarretando ao grupo.'
+      },
+      {
+        id: 'B',
+        text: 'Para não me aborrecer com ambos, simplesmente não faria qualquer comentário.'
+      }
+    ]
+  }
+]
