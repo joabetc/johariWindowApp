4d7ff3742a6f68ef319fe663335bdd6dfb4b3e34
Joabe Costa <joabetc@hotmail.com>
2018-04-23 23:54:43 -0300

    Adding questions to mock data

--- src/model/mock-questions.ts
+++ src/model/mock-questions.ts
@@ -2,19 +2,123 @@ import { Question } from "./question";
 
 export const QUESTIONS: Question[] = [
   {
-    id: 1,
-    text: 'Caso um colega tenha um mau relacionamento com outro colaborador, cuja cooperação é fundamental para o bom desempenho do grupo, eu...',
-    answers: [
-      {
-        id: 'A',
-        text: 'Falaria ao meu colega que ele é responsável em parte, pelo mau relacionamento com o outro e das consequências que isso está acarretando ao grupo.',
-        points: 0
-      },
-      {
-        id: 'B',
-        text: 'Para não me aborrecer com ambos, simplesmente não faria qualquer comentário.',
-        points: 0
-      }
-    ]
+    id: 1, text: 'Caso um colega tenha um mau relacionamento com outro colaborador, cuja cooperação é fundamental para o bom desempenho do grupo, eu: ', answers: [{
+      id: 'A', text: 'Falaria ao meu colega que ele é responsável em parte, pelo mau relacionamento com o outro e das conseqüências que isso está acarretando ao grupo.', points: 0
+    },
+    { id: 'B', text: 'Para não me aborrecer com ambos, simplesmente não faria qualquer comentário.', points: 0 }]
+  },
+  {
+    id: 2, text: 'Se eu tomasse conhecimento que um colega com o qual andei discutindo, está falando mal a meu respeito, eu: ', answers: [{
+      id: 'A', text: 'Não faria nada, a fim de evitar maiores complicações e brigas inúteis, até porque o tempo é o melhor remédio.', points: 0
+    },
+    { id: 'B', text: 'Diria a ele o quanto me desagrada a sua atitude e proporia que juntos, encontrássemos uma solução.', points: 0 }]
+  },
+  {
+    id: 3, text: 'Se percebo que meu colaborador está se comportando de uma maneira diferente, me evitando, fazendo seu trabalho, mas distante de mim, eu: ', answers: [{
+      id: 'A ', text: 'Perguntaria a ele o que esta acontecendo.', points: 0
+    },
+    { id: 'B ', text: 'Se ele prefere esse tipo de relacionamento, também vou tratá-lo assim.', points: 0 }]
+  },
+  {
+    id: 4, text: 'Se durante uma reunião entre colegas de trabalho, o assunto abordado dirigese para uma área sobre a qual consideram-me grande conhecedor, mas que na verdade nada sei a respeito, eu: ', answers: [{
+      id: 'A', text: 'Procuro mudar o rumo do assunto, antes de solicitarem minha opinião.', points: 0
+    },
+    { id: 'B', text: 'Simplesmente confesso o meu desconhecimento, sem maiores preocupações e tento manter o assunto em pauta.', points: 0 }]
+  },
+  {
+    id: 5, text: 'Meu colega tenta alertar-me sobre o tipo de comportamento que estou apresentando e o prejuízo que isto está causando no meu relacionamento, com os demais membros da nossa equipe, eu: ', answers: [{
+      id: 'A', text: 'Pediria que ele fornecesse maiores detalhes a respeito do meu comportamento e também, que apresentasse sugestões para melhorar meu convívio com os outros.', points: 0
+    },
+    { id: 'B', text: 'Procuraria demonstrar-lhe porque me comporto da forma que ele está dizendo.', points: 0 }]
+  },
+  {
+    id: 6, text: 'Meu chefe está sendo convidado pela direção da empresa para ocupar um cargo maior, porém acho que ele não reúne qualificações para o cargo, eu: ', answers: [{
+      id: 'A', text: 'Não faria comentários com meu chefe e com a direção a respeito do que penso, pois isso deve ser decisão deles.', points: 0
+    },
+    { id: 'B', text: 'Manifestaria minha preocupação para meu chefe e também para a direção, a fim de que eles examinassem melhor essa decisão.', points: 0 }]
+  },
+  {
+    id: 7, text: 'Caso eu sentisse que o tratamento que meu chefe está me dispensando é injusto, eu: ', answers: [{
+      id: 'A', text: ' Perguntaria a ele o que está ocorrendo, o que estou fazendo para merecer esse tratamento injusto.', points: 0
+    },
+    { id: 'B', text: 'Não faria quaisquer comentários e aguardaria que ele abordasse o assunto.', points: 0 }]
+  },
+  {
+    id: 8, text: 'Caso eu estivesse enfrentando problemas pessoais e meu superior me alertasse que nos últimos tempos tenho sido muito agressivo, eu: ', answers: [{
+      id: 'A', text: 'Pediria a ele que me deixasse em paz, porque estou passando por um momento difícil. B( ) – Escutaria suas observações e não faria qualquer comentário sobre o que está ocorrendo comigo.', points: 0
+    },
+    { id: 'B', text: 'Informaria a ele tudo o que ouvi e quem me informou.', points: 0 }]
+  },
+  {
+    id: 9, text: 'Caso meu colega de trabalho viesse me perguntar se ouvi boatos a seu respeito, que podem prejudicá-lo, e que de fato já tomei conhecimento, eu: ', answers: [{
+      id: 'A', text: 'Diria a ele que nada sei a respeito do assunto e que ele não desse a mínima importância, até porque “boatos são boatos”.', points: 0
+    },
+    { id: 'B', text: 'Informaria a ele tudo o que ouvi e quem me informou.', points: 0 }]
+  },
+  {
+    id: 10, text: 'Se um dos meus auxiliares fizesse um comentário, a respeito do mau relacionamento que tenho com outra pessoa do mesmo nível hierárquico que o meu, cuja colaboração é fundamental para a empresa, eu: ', answers: [{
+      id: 'A', text: 'Diria a ele que o assunto não me interessa e que ele cuide do seu trabalho.', points: 0
+    },
+    { id: 'B', text: 'O ouviria com atenção, discutiria cada detalhe e analisaria até que ponto esse problema está atrapalhando nosso rendimento.', points: 0 }]
+  },
+  {
+    id: 11, text: 'Caso o relacionamento que mantenho com meu superior estivesse sendo afetado, devido a freqüentes discussões que temos, a respeito de um assunto muito importante para nós, eu: ', answers: [{
+      id: 'A', text: 'Procuraria ser mais reservado e evitaria retomar ao assunto, afim de não prejudicar nosso relacionamento ainda mais.', points: 0
+    },
+    { id: 'B', text: ' Mostraria a ele o quanto nossas divergências estão atrapalhando nosso relacionamento e proporia discutir o assunto até a solução do mesmo.', points: 0 }]
+  },
+  {
+    id: 12, text: 'Caso durante uma reunião com meus colegas e meu chefe, um dos colegas propusesse que fosse avaliado o desempenho do meu setor/atividade, eu: ', answers: [{
+      id: 'A', text: 'Argumentaria que meu chefe é a pessoa mais habilitada para, sem ajuda de ninguém, avaliar meu desempenho.', points: 0
+    },
+    { id: 'B', text: 'Aceitaria de bom grado e incentivaria aos demais colegas, para que expressassem suas opiniões.', points: 0 }]
+  },
+  {
+    id: 13, text: 'Caso um colega mencionasse o seu desagrado em relação a outro membro do nosso grupo, que na sua opinião tem tido um mau relacionamento com todos (opinião com a qual concordo plenamente), eu: ', answers: [{
+      id: 'A', text: 'O escutaria, porém não manifestaria minha opinião, a fim de evitar maiores confusões, caso ele não mantivesse sigilo sobre meus comentários.', points: 0
+    },
+    { id: 'B', text: 'O escutaria e manifestaria minha opinião, a fim de que ele soubesse o que penso sobre o assunto.', points: 0 }]
+  },
+  {
+    id: 14, text: 'Caso eu suspeitasse que um forte boato a meu respeito, está sendo divulgado na empresa e que um dos meus auxiliares, certamente está a par do assunto, eu: ', answers: [{
+      id: 'A', text: 'Não faria nenhum comentário e aguardaria que ele viesse informar-me sobre o assunto.', points: 0
+    },
+    { id: 'B', text: 'Comentaria com ele e o pressionaria para que ele me informasse sobre tudo que sabe a respeito do assunto.', points: 0 }]
+  },
+  {
+    id: 15, text: 'Caso, no dia-a-dia, eu percebesse que um dos meus colaboradores não estivesse se relacionando adequadamente com os demais colegas, eu: ', answers: [{
+      id: 'A', text: 'Não faria qualquer comentário, evitando intrometer-me no relacionamento do grupo.', points: 0
+    },
+    { id: 'B', text: 'Correria o risco de ser considerado ”intrometido” e lhe manifestaria o que tenho percebido, e também qual é minha opinião sobre o assunto.', points: 0 }]
+  },
+  {
+    id: 16, text: 'Se, ao conversar com dois de meus auxiliares, um deles divulgasse, inesperadamente, um projeto de alguns membros da equipe, o qual desconheço completamente e que poderia atrapalhar nosso rendimento, eu: ', answers: [{
+      id: 'A', text: 'Os colocaria “contra a parede”, para conseguir maiores informações sobre o projeto e também procuraria saber, o que eles pensam sobre o assunto.', points: 0
+    },
+    { id: 'B', text: 'Os deixaria a vontade para continuar falando ou não, a respeito desse projeto.', points: 0 }]
+  },
+  {
+    id: 17, text: 'Caso meu chefe demonstrasse sinais de estar sempre se preocupando e se irritando, por qualquer coisa comigo, eu: ', answers: [{
+      id: 'A', text: 'Seria muito habilidoso, ao lidar com ele, imaginando que ele deveria estar enfrentando algum problema de caráter pessoal.', points: 0
+    },
+    { id: 'B', text: 'Conversaria com ele a respeito do assunto e lhe diria o quanto a sua atitude está dificultando o nosso entendimento.', points: 0 }]
+  },
+  {
+    id: 18, text: 'Caso eu começasse a sentir desinteresse em trabalhar com meu chefe e percebesse que essa atitude estava interferindo no nosso relacionamento, eu: ', answers: [{
+      id: 'A', text: 'Não comentaria nada, porém permitiria que ele percebesse o que estaria acontecendo, através de um tratamento mais distante e falando com ele, apenas o indispensável sobre o trabalho.', points: 0
+    },
+    { id: 'B', text: 'Comunicaria diretamente o que estou sentindo e tentaria buscar o entendimento.', points: 0 }]
+  },
+  {
+    id: 19, text: 'Caso eu estivesse abordando o desempenho de um colega, com ele mesmo, que é uma pessoa extremamente sensível, eu: ', answers: [{
+      id: 'A', text: 'Para não melindrá-lo, evitaria citar seus pontos negativos.', points: 0
+    },
+    { id: 'B', text: 'O informaria sobre suas deficiências e o encorajaria a buscar um melhor desempenho.', points: 0 }]
+  },
+  {
+    id: 20, text: 'Caso eu tomasse conhecimento que estaria sendo avaliado para uma promoção na empresa e nessa oportunidade, meu relacionamento com o grupo estivesse bastante difícil, eu: ', answers: [{
+      id: 'A', text: 'Examinaria sozinho minhas dificuldades e tentaria encontrar alternativas para melhorar meu comportamento.', points: 0
+    },
+    { id: 'B', text: 'Abordaria com o grupo minhas deficiências e buscaria melhorar nosso relacionamento', points: 0 }]
   }
 ]
